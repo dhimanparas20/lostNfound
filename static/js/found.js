a53d3b9f9b447906e3d814c96f41e8ad56452d0e
Paras Dhiman <55368020+dhimanparas20@users.noreply.github.com>
2023-09-29 21:34:54 +0530

    Update found.js

--- static/js/found.js
+++ static/js/found.js
@@ -45,6 +45,11 @@ $(document).ready(function() {
                                         ? `<button class="btn btn-danger" type="submit" onclick="deleteItem('${item.id}') ">Delete</button>`
                                         : ''
                                 }
+                                ${
+                                    userAdmin === true && item.status === 1
+                                        ? `<button class="btn btn-info" type="submit" onclick="revertItem('${item.id}') ">Revert</button>`
+                                        : ''
+                                  }
                              </div>
                          </div>
                      `;
@@ -98,4 +103,20 @@ function deleteItem(itemId){
 
 }
 
-
+function revertItem(itemId){
+    $.ajax({
+        url: '/revert/',
+        method: 'POST',
+        data: {
+            itemId: itemId,
+            statusType: 'FOUND'
+          },
+        success: function(data) {
+            //console.log(data);
+            location.reload();
+        },
+        error: function(xhr, status, error) {
+            console.error('Error Sending Request', error);
+        }
+    });
+}
